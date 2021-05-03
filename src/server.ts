9739a7b8f7602fcec0930c092c6a266d6ffda4f5
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-05-03 17:40:51 -0300

    Teste de rotas com dados do banco

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,37 @@
 import express from 'express';
 import cors from 'cors';
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/test", (req, res) => {
-    return res.json({msg: "Hello World!"});
+app.get("/create", async (req, res) => {
+    const result = await prisma.series.create({
+        data: {
+            curso: "INFORMATICA",
+            ano: 2,
+            tipo: "MEDIOTECNICO",
+            periodo: "NOTURNO"
+        },
+    })
+
+    console.log(result);
+
+    return res.status(201).json({message: "OK"});
 });
+
+app.get('/list', async (req, res) => {
+    const results = await prisma.series.findMany({
+        where: {
+            ano: 2
+        }
+    });
+
+    return res.json(results);
+})
 
 app.listen(3334, () => console.log("Server is running..."));
