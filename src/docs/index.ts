af1c96c4c228bbaff22658019b97209754a6c1f0
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-05-16 22:06:39 -0300

    Mudanças na localização do arquivo

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -2,7 +2,7 @@ import {Request, Response} from 'express';
 import path from 'path';
 
 async function renderDocs(req:Request, res: Response) {
-    return res.sendFile(path.join(__dirname, 'index.html'));
+    return res.sendFile(path.join(__dirname, '..','..','index.html'));
 }
 
 export default renderDocs;
