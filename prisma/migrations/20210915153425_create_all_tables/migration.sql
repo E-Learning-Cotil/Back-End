d5480a34beacea6e199ad6a25c2539687e0cdb21
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-09-15 15:52:14 -0300

    Adicionado ícones brancos

--- prisma/migrations/20210915153425_create_all_tables/migration.sql
+++ prisma/migrations/20210915153425_create_all_tables/migration.sql
@@ -168,6 +168,7 @@ CREATE TABLE `ArquivosAtividades` (
 CREATE TABLE `Icones` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `link` VARCHAR(191) NOT NULL,
+    `altLink` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -146,24 +146,132 @@ async function main() {
 
 	const createManyIcones = await prisma.icones.createMany({
 		data: [
-			{
-				link: "https://i.imgur.com/yD06uw2.png"
-			},
-			{
-				link: "https://imgur.com/NpzPrQ4.png"
-			},
-			{
-				link: "https://imgur.com/XORThTC.png"
-			},
-			{
-				link: "https://imgur.com/LfUQhIV.png"
-			},
-			{
-				link: "https://imgur.com/FL9s1uY.png"
-			},
-			{
-				link: "https://imgur.com/IxuH2zj.png"
-			}
+			{ link: "https://imgur.com/hI42Ima.png", altLink: "https://imgur.com/sLFPBVG.png"}, //Cadeira de rodas
+			{ link: "https://imgur.com/RqB36Ic.png", altLink: "https://imgur.com/qABVeOS.png"}, //Alergia
+			{ link: "https://imgur.com/ICxvJ0R.png", altLink: "https://imgur.com/0PgKx6m.png"}, //Língua de sinais
+			{ link: "https://imgur.com/M5rQU9k.png", altLink: "https://imgur.com/Qu6Bu8l.png"}, //Android
+			{ link: "https://imgur.com/bEzrJ51.png", altLink: "https://imgur.com/TFReefH.png"}, //Maçã
+			{ link: "https://imgur.com/49bM31x.png", altLink: "https://imgur.com/oz3Ef6Y.png"}, //Bebê
+			{ link: "https://imgur.com/q1Z4xAq.png", altLink: "https://imgur.com/y19mjT2.png"}, //Bactérias
+			{ link: "https://imgur.com/mFdPQwa.png", altLink: "https://imgur.com/4Pf2Ecx.png"}, //Justiça
+			{ link: "https://imgur.com/MvQx25I.png", altLink: "https://imgur.com/I1WMz2U.png"}, //Mini primeiros socorros
+			{ link: "https://imgur.com/D2NvXNo.png", altLink: "https://imgur.com/5eMY7Ve.png"}, //Basquete
+			{ link: "https://imgur.com/lRHSpbP.png", altLink: "https://imgur.com/wmARD9H.png"}, //Cama
+			{ link: "https://imgur.com/nkdyWtK.png", altLink: "https://imgur.com/EqPCzWA.png"}, //Bíblia
+			{ link: "https://imgur.com/KTxFezy.png", altLink: "https://imgur.com/Oq7aBwb.png"}, //Bitcoin
+			{ link: "https://imgur.com/RwAqO6d.png", altLink: "https://imgur.com/gUAMj77.png"}, //Osso
+			{ link: "https://imgur.com/LQJRqMt.png", altLink: "https://imgur.com/0I6aNQN.png"}, //Livro de medicina
+			{ link: "https://imgur.com/LQJRqMt.png", altLink: "https://imgur.com/0I6aNQN.png"}, //Livro de medicina
+			{ link: "https://imgur.com/zUC9ayI.png", altLink: "https://imgur.com/NpnDwvC.png"}, //Braille
+			{ link: "https://imgur.com/p5zwIzA.png", altLink: "https://imgur.com/T8vVzgP.png"}, //Cérebro
+			{ link: "https://imgur.com/5cCh94l.png", altLink: "https://imgur.com/zn0sS8d.png"}, //Maleta
+			{ link: "https://imgur.com/mjmcPrq.png", altLink: "https://imgur.com/vAfnx6z.png"}, //Inseto
+			{ link: "https://imgur.com/tuEMyos.png", altLink: "https://imgur.com/KxDgsiO.png"}, //Prédio
+			{ link: "https://imgur.com/KZ0j8RR.png", altLink: "https://imgur.com/orL9tEB.png"}, //Calendário
+			{ link: "https://imgur.com/NE9yG0N.png", altLink: "https://imgur.com/RjnRHap.png"}, //Câmera
+			{ link: "https://imgur.com/MjOss7B.png", altLink: "https://imgur.com/lNkBi6V.png"}, //Bengala doce
+			{ link: "https://imgur.com/3R9E238.png", altLink: "https://imgur.com/wvmL3qf.png"}, //Pílulas
+			{ link: "https://imgur.com/XPi2row.png", altLink: "https://imgur.com/Hafggk1.png"}, //Carro
+			{ link: "https://imgur.com/F98AU3Z.png", altLink: "https://imgur.com/GFDOPf9.png"}, //Cenoura
+			{ link: "https://imgur.com/WX7CiPD.png", altLink: "https://imgur.com/oXPNStr.png"}, //Professor e lousa
+			{ link: "https://imgur.com/C8As9qC.png", altLink: "https://imgur.com/LKH1iBd.png"}, //Estatísticas
+			{ link: "https://imgur.com/CqeYgGQ.png", altLink: "https://imgur.com/7c3GBZ8.png"}, //Xadrez
+			{ link: "https://imgur.com/S3xLkt8.png", altLink: "https://imgur.com/Ttc4qZF.png"}, //Criança
+			{ link: "https://imgur.com/N7QEwIL.png", altLink: "https://imgur.com/xkg48Dx.png"}, //Cidade
+			{ link: "https://imgur.com/8AFXjY2.png", altLink: "https://imgur.com/uabF0Vi.png"}, //Prancheta
+			{ link: "https://imgur.com/xoe8JLN.png", altLink: "https://imgur.com/IzdzQum.png"}, //Clima
+			{ link: "https://imgur.com/s7CYzIZ.png", altLink: "https://imgur.com/o4pyO5W.png"}, //Código
+			{ link: "https://imgur.com/259woRj.png", altLink: "https://imgur.com/bJvW9ph.png"}, //Muleta
+			{ link: "https://imgur.com/y8bUlK3.png", altLink: "https://imgur.com/O3mXNak.png"}, //Banco de dados
+			{ link: "https://imgur.com/mRULZDy.png", altLink: "https://imgur.com/WnqCHuk.png"}, //Monitor
+			{ link: "https://imgur.com/PM6Jk1k.png", altLink: "https://imgur.com/bZMLnAd.png"}, //Germes
+			{ link: "https://imgur.com/UtQzLSk.png", altLink: "https://imgur.com/oVYXEaE.png"}, //Divisão
+			{ link: "https://imgur.com/w9Z95Pw.png", altLink: "https://imgur.com/qdgabqO.png"}, //DNA
+			{ link: "https://imgur.com/JfJVTsP.png", altLink: "https://imgur.com/jHIs9Ch.png"}, //Cachorro
+			{ link: "https://imgur.com/l2k6Kab.png", altLink: "https://imgur.com/5kV7SZo.png"}, //Dinheiro
+			{ link: "https://imgur.com/Fg5sLPu.png", altLink: "https://imgur.com/pAvsTe5.png"}, //Pássaro
+			{ link: "https://imgur.com/tJ3ppOX.png", altLink: "https://imgur.com/RSQPwka.png"}, //Bússola
+			{ link: "https://imgur.com/31r9Qsl.png", altLink: "https://imgur.com/nkUzzqW.png"}, //Tambor
+			{ link: "https://imgur.com/fkC9ySq.png", altLink: "https://imgur.com/kFb4Rdt.png"}, //Frango
+			{ link: "https://imgur.com/VvEIiPV.png", altLink: "https://imgur.com/RdlJNaY.png"}, //Folha de árvore
+			{ link: "https://imgur.com/j1vBkFI.png", altLink: "https://imgur.com/3dgarPP.png"}, //Olho
+			{ link: "https://imgur.com/13TKLPv.png", altLink: "https://imgur.com/D8leDXu.png"}, //Catavento
+			{ link: "https://imgur.com/5wtiNcA.png", altLink: "https://imgur.com/55uzGvw.png"}, //Arquivo
+			{ link: "https://imgur.com/vPghBcs.png", altLink: "https://imgur.com/kAwn5qC.png"}, //Filme
+			{ link: "https://imgur.com/s3L6ULg.png", altLink: "https://imgur.com/7DFqrry.png"}, //Fogo
+			{ link: "https://imgur.com/EGn17dZ.png", altLink: "https://imgur.com/8WKxHa8.png"}, //Extintor 
+			{ link: "https://imgur.com/ukShPWH.png", altLink: "https://imgur.com/iGbaBVG.png"}, //Primeiros socorros
+			{ link: "https://imgur.com/X1k2lva.png", altLink: "https://imgur.com/se7ICN3.png"}, //Peixe
+			{ link: "https://imgur.com/UtXPumM.png", altLink: "https://imgur.com/qgAqB16.png"}, //Erlenmeyer
+			{ link: "https://imgur.com/acef1ts.png", altLink: "https://imgur.com/i4jeNq9.png"}, //Sapo
+			{ link: "https://imgur.com/TBuYOwr.png", altLink: "https://imgur.com/zgke9vE.png"}, //Jogos
+			{ link: "https://imgur.com/lliy56B.png", altLink: "https://imgur.com/oG9IL4d.png"}, //Diamante
+			{ link: "https://imgur.com/8YkMkJY.png", altLink: "https://imgur.com/hdmWf0I.png"}, //Planeta Terra
+			{ link: "https://imgur.com/WCX1HNq.png", altLink: "https://imgur.com/xYQOLOQ.png"}, //Globo
+			{ link: "https://imgur.com/1kWYUjE.png", altLink: "https://imgur.com/wOQKKbq.png"}, //Violão
+			{ link: "https://imgur.com/KxDSkG6.png", altLink: "https://imgur.com/sLfzXib.png"}, //Martelo
+			{ link: "https://imgur.com/WPOCwZd.png", altLink: "https://imgur.com/9oCt9IU.png"}, //Capacete de segurança
+			{ link: "https://imgur.com/j5xaDF4.png", altLink: "https://imgur.com/GWZmz6A.png"}, //Disco rígido
+			{ link: "https://imgur.com/ABFeb56.png", altLink: "https://imgur.com/FkcEQcr.png"}, //Pessoa com máscara
+			{ link: "https://imgur.com/NZ5eoen.png", altLink: "https://imgur.com/JOSBqlD.png"}, //Fone de ouvido
+			{ link: "https://imgur.com/Jvliz6x.png", altLink: "https://imgur.com/gS4nzyM.png"}, //Helicóptero
+			{ link: "https://imgur.com/7WUnaSk.png", altLink: "https://imgur.com/mxAjBsv.png"}, //Hospital
+			{ link: "https://imgur.com/IbXDDBr.png", altLink: "https://imgur.com/WMoxjE2.png"}, //Imagem
+			{ link: "https://imgur.com/h1iiU6i.png", altLink: "https://imgur.com/kkZCv8G.png"}, //Indústria
+			{ link: "https://imgur.com/v8hyq2J.png", altLink: "https://imgur.com/zl8IIAc.png"}, //Teclado
+			{ link: "https://imgur.com/H5aZIOj.png", altLink: "https://imgur.com/ejsOyNm.png"}, //Notebook com código
+			{ link: "https://imgur.com/LHgcDQw.png", altLink: "https://imgur.com/WQPCcW1.png"}, //Notebook
+			{ link: "https://imgur.com/ztCteCp.png", altLink: "https://imgur.com/l2lXgA1.png"}, //Lâmpada
+			{ link: "https://imgur.com/P8tSeqD.png", altLink: "https://imgur.com/OdqdrSN.png"}, //Pulmões
+			{ link: "https://imgur.com/fI04qrC.png", altLink: "https://imgur.com/YFd0h3e.png"}, //Mapa
+			{ link: "https://imgur.com/nMR4lJx.png", altLink: "https://imgur.com/kfVu4K3.png"}, //Placas/Rotas
+			{ link: "https://imgur.com/7V2VfdU.png", altLink: "https://imgur.com/LlN27no.png"}, //MicroChip
+			{ link: "https://imgur.com/a5Ek25r.png", altLink: "https://imgur.com/8aGV3HF.png"}, //Microfone
+			{ link: "https://imgur.com/cDlZYuw.png", altLink: "https://imgur.com/3SFq1rm.png"}, //Microscópio
+			{ link: "https://imgur.com/Def3xJr.png", altLink: "https://imgur.com/tTXKv0e.png"}, //Celular
+			{ link: "https://imgur.com/Sgjivf0.png", altLink: "https://imgur.com/jFuEmXv.png"}, //Jornal
+			{ link: "https://imgur.com/MqTnzXF.png", altLink: "https://imgur.com/eCZhrkT.png"}, //Pincel
+			{ link: "https://imgur.com/duj0558.png", altLink: "https://imgur.com/yBZQMwq.png"}, //Paleta de cores
+			{ link: "https://imgur.com/0RTUryf.png", altLink: "https://imgur.com/lX1HFSX.png"}, //Atlas
+			{ link: "https://imgur.com/vdGcx6w.png", altLink: "https://imgur.com/gj7FI90.png"}, //Lápis
+			{ link: "https://imgur.com/ddOkX65.png", altLink: "https://imgur.com/H5eB4DY.png"}, //Porcentagem
+			{ link: "https://imgur.com/FSMrVnA.png", altLink: "https://imgur.com/FbDBQ88.png"}, //Foto-Vídeo
+			{ link: "https://imgur.com/rVWqogt.png", altLink: "https://imgur.com/ZYhIrNr.png"}, //Remédios
+			{ link: "https://imgur.com/4Zwe3ic.png", altLink: "https://imgur.com/KoSGqld.png"}, //Avião
+			{ link: "https://imgur.com/KYdUCe5.png", altLink: "https://imgur.com/4RqJXpE.png"}, //Tomada
+			{ link: "https://imgur.com/JVmkCAG.png", altLink: "https://imgur.com/4zPWUBi.png"}, //Pote de remédios
+			{ link: "https://imgur.com/IBI9d4S.png", altLink: "https://imgur.com/oWMgiTJ.png"}, //Átomo
+			{ link: "https://imgur.com/NxdvFSN.png", altLink: "https://imgur.com/NKZRXAt.png"}, //Robô
+			{ link: "https://imgur.com/5MB5Eod.png", altLink: "https://imgur.com/oExSe5l.png"}, //Foguete
+			{ link: "https://imgur.com/2ZBJwCO.png", altLink: "https://imgur.com/SuIMBV8.png"}, //Régua
+			{ link: "https://imgur.com/K0pWjdM.png", altLink: "https://imgur.com/hS6eNLw.png"}, //Formas geométricas
+			{ link: "https://imgur.com/u5pCxMQ.png", altLink: "https://imgur.com/MbsOhcT.png"}, //Imunização
+			{ link: "https://imgur.com/zrtgPfz.png", altLink: "https://imgur.com/WdIOsQy.png"}, //Van
+			{ link: "https://imgur.com/iZxIcgX.png", altLink: "https://imgur.com/Mz3nrch.png"}, //Diagrama
+			{ link: "https://imgur.com/zzL9355.png", altLink: "https://imgur.com/68rZNHb.png"}, //Caveira
+			{ link: "https://imgur.com/gy0bKSX.png", altLink: "https://imgur.com/hONlybV.png"}, //Floco de neve
+			{ link: "https://imgur.com/foYg5yw.png", altLink: "https://imgur.com/ETne5kF.png"}, //Sabonete
+			{ link: "https://imgur.com/rl8wzn0.png", altLink: "https://imgur.com/UoWmZud.png"}, //Placa solar
+			{ link: "https://imgur.com/TLGsHAE.png", altLink: "https://imgur.com/vmjaV0v.png"}, //Aranha
+			{ link: "https://imgur.com/WAgvwi0.png", altLink: "https://imgur.com/htKaFKu.png"}, //Perfume
+			{ link: "https://imgur.com/lqZCwxN.png", altLink: "https://imgur.com/JTEsTam.png"}, //Raíz quadrada
+			{ link: "https://imgur.com/vC9lxvs.png", altLink: "https://imgur.com/U2SeEP8.png"}, //Estetoscópio
+			{ link: "https://imgur.com/60cOS6g.png", altLink: "https://imgur.com/mFlxwfc.png"}, //Natação
+			{ link: "https://imgur.com/CZlX1RH.png", altLink: "https://imgur.com/3cNix3I.png"}, //Tablet
+			{ link: "https://imgur.com/xgO0PAU.png", altLink: "https://imgur.com/Uu5SUN0.png"}, //Dentes
+			{ link: "https://imgur.com/1T2sQIF.png", altLink: "https://imgur.com/uLE1Tav.png"}, //Termômetro
+			{ link: "https://imgur.com/20MXVeR.png", altLink: "https://imgur.com/JdfqBgr.png"}, //Teatro
+			{ link: "https://imgur.com/ao5xEB5.png", altLink: "https://imgur.com/aRDP0KU.png"}, //Ferramentas
+			{ link: "https://imgur.com/4eg7VAI.png", altLink: "https://imgur.com/JvxhbBb.png"}, //Dente
+			{ link: "https://imgur.com/wHeiTf3.png", altLink: "https://imgur.com/BWLoo9S.png"}, //Semáforo
+			{ link: "https://imgur.com/0IWSbP6.png", altLink: "https://imgur.com/vavTUYR.png"}, //Pinheiro
+			{ link: "https://imgur.com/fb6lRKd.png", altLink: "https://imgur.com/ux6iF2f.png"}, //Camiseta
+			{ link: "https://imgur.com/i9TH9pC.png", altLink: "https://imgur.com/8Xe04mU.png"}, //TV
+			{ link: "https://imgur.com/PTjeqT6.png", altLink: "https://imgur.com/VAtQFOX.png"}, //Talheres
+			{ link: "https://imgur.com/wDpYzaX.png", altLink: "https://imgur.com/ATGSnPl.png"}, //Tubo de ensaio
+			{ link: "https://imgur.com/gh5mcZm.png", altLink: "https://imgur.com/KnITmGQ.png"}, //Gravação
+			{ link: "https://imgur.com/X8hRZKz.png", altLink: "https://imgur.com/oWT1snw.png"}, //Vírus
+			{ link: "https://imgur.com/M8bmglI.png", altLink: "https://imgur.com/88TkZOU.png"}, //Chave de boca
 		]
 	})
 
