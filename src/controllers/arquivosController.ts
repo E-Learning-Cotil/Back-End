2d6e727e136d64afa5af2e0a91562eaa5a98b93c
Eduardo Correia <60967470+eduardo-ehsc@users.noreply.github.com>
2021-10-04 11:34:22 -0300

    Update arquivosController.ts

--- src/controllers/arquivosController.ts
+++ src/controllers/arquivosController.ts
@@ -6,7 +6,8 @@ class ArquivosController{
     async upload(req: Request, res: Response, next: NextFunction){
         try {
             Cloudinary.uploader.upload(req.file.path, function(error, result) {
-                return res.status(200).json(result.url);
+		console.log(result);
+                return res.status(200).json(result);
             });
 		} catch (error) {
 			const err = new InternalError('Falha ao subir um arquivo!', 400, error.message);
